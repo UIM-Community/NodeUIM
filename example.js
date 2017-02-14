856c9940f767d7630c182b2906a57ed0c0d4c44d
Gentilhomme <gentilhomme.thomas@gmail.com>
2017-02-14 11:25:47 +0100

    Update for release 1

--- example.js
+++ example.js
@@ -1,29 +1,31 @@
 const SDK       = require('./nimsoft.js');
+const Nimsoft   = SDK.Nimsoft;
+const Logger    = SDK.Logger;
 const config    = require('./configuration.json');
 const async     = require('async');
 const fs        = require('fs');
 
-const logger = new SDK.Logger({
+const logger = new Logger({
     file: 'nodepu.log'
 });
-const nimSDK = new SDK.Nimsoft(config);
-const errorHandler = function(Err) {
-    logger.log("ERROR: "+Err);
-}
+const nimSDK = new Nimsoft(config);
 nimSDK.on('log', msg => logger.log(msg) );
 
+function closeHandler() {
+    logger.log(console.timeEnd('write_name'));
+    logger.close();
+    process.exit(0);
+}
+
 const RobotsList = [];
-var i = 0;
 
 logger.log(console.time('write_name'));
 logger.log('Get robots!');
 logger.log('---------------------------------');
-nimSDK.request({ callback: 'gethubs' })
-.then( HubMap => {
-    return new Promise( (resolve,reject) => {
-
-        const Hubs = HubMap.get('hublist');
-        async.each(Hubs,(hub,donehub) => {
+nimSDK.pu({ callback: 'gethubs' }).then( Response => {
+    return new Promise( (resolve, reject) => {
+        logger.log('Return code => '+Response.rc);
+        async.each(Response.Map.get('hublist'),(hub, donehub) => {
             if(hub.domain !== 'NMS-PROD') {
                 donehub();
                 return;
@@ -31,38 +33,38 @@ nimSDK.request({ callback: 'gethubs' })
             logger.log(`hubName : ${hub.name}`);
             logger.log('---------------------------------');
             
-            nimSDK.request({ path: hub.addr, callback: 'getrobots', args: [SDK.Nimsoft.NoARG,SDK.Nimsoft.NoARG]})
-            .then( RobotMap => {
-
-                const Robots = RobotMap.get('robotlist');
-                async.each(Robots,(robot,donerobot) => {
-                    i++;
+            nimSDK.pu({ path: hub.addr, callback: 'getrobots', args: [Nimsoft.noArg,Nimsoft.noArg]}).then( ResponseSub => {
+                logger.log('Return code => '+ResponseSub.rc);
+                async.each(ResponseSub.Map.get('robotlist'),(robot, donerobot) => {
                     RobotsList.push(robot.name);
                     donerobot();
                 } , err => {
-                    if(err) console.log(err);
+                    err && logger.log(err);
                     logger.log(`${hub.addr} OK !!`);
                     donehub();
                 });
-
-            }).catch( errorHandler ); 
+            }).catch( ProbeUtility => {
+                logger.log(ProbeUtility.error+` for ${hub.name}`,Logger.Error);
+                donehub();
+            }); 
 
         }, err => {
-            if(err) logger.log(`critical: ${err}`);
+            err && logger.log(err,Logger.Error);
             resolve(true);
         });
 
     } );
 })
 .then( _ => {
-    logger.log(`Processing done ! Robots size => ${RobotsList.length} and i size => ${i}`);
-    fs.writeFile('robotslist.txt',RobotsList.join("\r\n"),function(err) {
+    logger.log(`Processing done ! Robots size => ${RobotsList.length}`);
+    fs.writeFile('robotslist.txt',RobotsList.join("\r\n"), err => {
         if(err) {
             throw new Error(err);
         }
-        logger.log(console.timeEnd('write_name'));
-        logger.close();
-        process.exit(0);
+        closeHandler();
     });
 })
-.catch( errorHandler );
+.catch( ProbeUtility => {
+    logger.log(ProbeUtility.error,Logger.Error);
+    closeHandler();
+});
